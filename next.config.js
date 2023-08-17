f4f628969d57e444ccb8d74be1a2f9aa273f33a7
ykn9080 <ykn9080@empal.com>
2023-08-17 19:41:59 +0900

    dockerfile added

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   },
 
   // output: "export",
+  //output: "standalone",
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
