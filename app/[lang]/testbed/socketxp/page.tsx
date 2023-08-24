b26bf906fb9c5e7151a5208102383ea41f57ebda
ykn9080 <ykn9080@empal.com>
2023-08-24 19:50:05 +0900

    next to node test

--- app/[lang]/testbed/socketxp/page.tsx
+++ app/[lang]/testbed/socketxp/page.tsx
@@ -1,31 +1,47 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, FormEvent } from "react";
 import executeSocketXp from "@/lib/socketxp";
-import type { NextPage } from "next";
-import { useForm } from "react-hook-form";
 
-const Form: NextPage = () => {
-  const { register, handleSubmit } = useForm();
-const onSubmit = async (data: object) => {
-  const response = await saveFormData(data);
-  if (response.status === 400) {
-    // Validation error
-  } else if (response.ok) {
-    // successful
-  } else {
-    // unknown error
-  }
-};
+export default async function SocketXp() {
+  const [command, setCommand] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const rtn = await executeSocketXp(
+      // "echo 'This is the content of the file.' > /home/yknam/helloconfig"
+      //"/usr/local/hadoop/bin/hadoop jar /home/yknam/IdeaProjects/mapreduce/target/mapreduce-1.0.0.jar /data/wordSample.txt /output"
+      //"/usr/local/hadoop/bin/hadoop fs -rm -r /outpu"
+      command
+    );
+    setResult(rtn);
+  };
 
   return (
-    <form onSubmit={handleSubmit(saveFormData)}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        autoComplete="email"
-        {...register("email", { required: true })}
-      />
-      <button>Submit</button>
-    </form>
+    <div className="container mx-auto">
+      <h1>Command</h1>
+      {/* 👇 wire-up the handleSubmit handler */}
+      <form
+        className="w-50 flex justify-center md:justify-between"
+        onSubmit={handleSubmit}
+      >
+        <input
+          placeholder="Enter your command script"
+          name="command"
+          className="bg-white p-2 w-80 text-xl rounded-xl"
+          value={command}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setCommand(e.target.value);
+          }}
+        />
+        <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+          Send
+        </button>
+      </form>
+      {/* show the data returned by the api */}
+      Result
+      <pre>{JSON.stringify(result, null, 4)}</pre>
+    </div>
   );
-};
+}
