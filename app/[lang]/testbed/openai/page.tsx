7b2b27609fd88019901f102fb6485fd3ef356845
ykn9080 <ykn9080@empal.com>
2023-12-06 18:44:34 +0900

    openai

--- app/[lang]/testbed/openai/page.tsx
+++ app/[lang]/testbed/openai/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import MyListBox from "../../components/ListBox";
 import { updateValue } from "@/redux/features/globalSlice";
-import { openaiCall } from "@/lib/openai";
+import { openaiCall, dalliCall, chatCall } from "@/lib/openai";
 import { useAppSelector } from "@/redux/hooks";
 
 export default function Page({ tags }: any) {
@@ -32,5 +32,85 @@ export default function Page({ tags }: any) {
       </form>
       <div>{rspMsg}</div>
     </div>
+  );
+}
+
+export function Dalli({ prompt }: any) {
+  const [promptname, setPromptname] = useState("");
+  const [currentMsg, setCurrentMsg] = useState("tiger with trainer");
+  const [rspMsg, setRspMsg] = useState("");
+
+  const sendData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const rtn = await dalliCall({ prompt: currentMsg });
+    console.log(rtn);
+    setRspMsg(rtn.data[0].url);
+  };
+  return (
+    <div>
+      <form onSubmit={(e) => sendData(e)}>
+        {/* <MyListBox tags={tags} /> */}
+        <input
+          type="text"
+          value={currentMsg}
+          placeholder="Type your message.."
+          onChange={(e) => setCurrentMsg(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
+      <div>
+        <img src={rspMsg} />
+      </div>
+    </div>
+  );
+}
+
+export function Chatbot({ prompt }: any) {
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content:
+        "You're like a grammar-checking wizard, helping users fix grammar bloopers and jazz up their sentence structures.",
+    },
+  ]);
+
+  const [currentMsg, setCurrentMsg] = useState("tiger with trainer");
+  const [rspMsg, setRspMsg] = useState("");
+  const handleSendMessage = (messageContent: string) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", content: messageContent },
+    ]);
+  };
+  const sendData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currentMsg.trim() !== "") {
+      handleSendMessage(currentMsg);
+      setCurrentMsg("");
+    }
+    const rtn = await chatCall({ prompt: messages });
+    setRspMsg(rtn.choices[0].message.content);
+  };
+  return (
+    <>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <h3>{message.role}</h3>
+            <p>{message.content}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={(e) => sendData(e)}>
+        <input
+          type="text"
+          name="input"
+          value={currentMsg}
+          placeholder="Type your message..."
+          onChange={(e) => setCurrentMsg(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </>
   );
 }
--- lib/openai.ts
+++ lib/openai.ts
@@ -1,3 +1,5 @@
+const API_KEY = process.env.REACT_APP_OPEN_API_KEY;
+
 export async function openaiCall(script: any) {
   console.log(script);
   const res = await fetch(
@@ -16,3 +18,77 @@ export async function openaiCall(script: any) {
   console.log(script, res);
   return res.json();
 }
+
+export async function dalliCall(script: any) {
+  console.log(script);
+  const prompt = { prompt: script, n: 1, size: "256x256" };
+  const res = await fetch(
+    "https://api.openai.com/v1/images/generations",
+
+    {
+      method: "POST",
+      body: JSON.stringify(prompt),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      cache: "no-store",
+    }
+  );
+  console.log(prompt, res);
+  return res.json();
+}
+
+export async function chatCall(messages: any) {
+  const response = await fetch(
+  "https://api.openai.com/v1/chat/completions",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      // We'll later replace the content with user input
+      messages: [...messages, { "role": "user", "content": "This is a test!" }],
+      temperature: 0.7,
+    }),
+  }
+);
+
+export const chatData = async (userMessage:string) => {
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [...messages, { role: "user", content: userMessage }],
+          temperature: 0.7,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Oops! Something went wrong while processing your request.");
+    }
+
+    const responseData = await response.json();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        role: "assistant",
+        content: responseData.choices[0].message.content,
+      },
+    ]);
+  } catch (error) {
+    console.error("Error while fetching chat data:", error);
+  }
+};
