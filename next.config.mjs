4b142991269cb89fadd727b0a938d9a8a4f2cf04
ykn9080 <ykn9080@empal.com>
2024-02-14 14:51:58 +0900

    package

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,8 @@ import remarkToc from "remark-toc";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-    outputStandalone: true,
-  },
-
   // output: "export",
-  //output: "standalone",
+  output: "standalone",
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
